133d9d66d36750db308acadf8759864ef89470e9
EliDevToBe <158275307+EliDevToBe@users.noreply.github.com>
2026-04-14 22:33:33 +0200

    fix (deploy): Generate prisma then build

--- prisma/seed/main.ts
+++ prisma/seed/main.ts
@@ -1,8 +1,8 @@
 import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
 import pg from "pg";
-import { seasonYearFromDate } from "../../domain/utils/index.js";
 import { validateParticipationRules } from "../../domain/participations/participation.rules";
+import { seasonYearFromDate } from "../../domain/utils/index.js";
 import { type Prisma, PrismaClient } from "../../generated/prisma/client.js";
 import { seedLinkedArchers, seedUnlinkedArcherNames } from "./data/archers.js";
 import { seedCompetitions } from "./data/competitions.js";
