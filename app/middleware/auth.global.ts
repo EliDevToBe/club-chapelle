96585e4bd6b272af53ec8e13a97fb22733a67c2a
EliDevToBe <158275307+EliDevToBe@users.noreply.github.com>
2026-04-22 01:41:02 +0200

    feat: auth router front

--- app/middleware/auth.global.ts
+++ app/middleware/auth.global.ts
@@ -1,15 +1,18 @@
 import { useAuthUser } from "~/composables/useAuthUser";
 
-export default defineNuxtRouteMiddleware(async (to, from) => {
-  const { user } = useAuthUser();
-
-  console.log(to, from);
+export default defineNuxtRouteMiddleware(async (to, _from) => {
+  const { user, isAdmin } = useAuthUser();
 
   const adminRoutes = ["/club"];
   const authRequiredRoutes = [...adminRoutes];
 
   // Global auth required routes
   if (!user.value && authRequiredRoutes.includes(to.path)) {
+    return navigateTo("/");
+  }
+
+  // Admin routes require admin role
+  if (!isAdmin.value && adminRoutes.includes(to.path)) {
     return navigateTo("/");
   }
 });
