87e5992ab68569fb62b6a07917e6357b5e165da1
EliDevToBe <158275307+EliDevToBe@users.noreply.github.com>
2026-06-28 21:37:07 +0200

    date util

---
+++ app/utils/index.ts
@@ -0,0 +1,9 @@
+import type { CalendarDate } from "@internationalized/date";
+
+export const calendarDateToYmd = (date?: CalendarDate): string | undefined => {
+  if (!date) return undefined;
+  const year = String(date.year).padStart(4, "0");
+  const month = String(date.month).padStart(2, "0");
+  const day = String(date.day).padStart(2, "0");
+  return `${year}-${month}-${day}`;
+};
