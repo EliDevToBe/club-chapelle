58cef29b7236c44c11c8c7b312f11ca6ac8a28ec
EliDevToBe <158275307+EliDevToBe@users.noreply.github.com>
2026-06-29 00:23:35 +0200

    fix: Content page x padding

--- app/components/layout/ContentPageWrapper.vue
+++ app/components/layout/ContentPageWrapper.vue
@@ -9,7 +9,7 @@ const ui = {
   root: [
     "flex flex-col w-full",
     "max-w-5xl",
-    "mx-auto px-4 py-8 md:py-12",
+    "mx-auto px-4 sm:px-8 py-8 md:py-12",
     "gap-9 md:gap-12",
     "text-muted",
   ],
