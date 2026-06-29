3b07e2180f4b6a1909c3d2b1fd41ff587923d50a
EliDevToBe <158275307+EliDevToBe@users.noreply.github.com>
2026-06-29 03:36:48 +0200

    feat (FF): FeatureManagement panel

---
+++ app/components/admin/FeatureManagementAccordion.vue
@@ -0,0 +1,28 @@
+<template>
+  <ChapAccordion
+    :items="accordionItems"
+    v-model="selectedItem"
+    class="transition-colors duration-150"
+    :class="selectedItem ? 'bg-secondary/5' : ''"
+  >
+    <template #content="{ item }">
+      <FeatureManagement v-if="item.value === 'feature-flags'" />
+    </template>
+  </ChapAccordion>
+</template>
+
+<script setup lang="ts">
+import type { AccordionItem } from "@nuxt/ui";
+import FeatureManagement from "~/components/admin/FeatureManagement.vue";
+import ChapAccordion from "~/components/ui/ChapAccordion.vue";
+
+const accordionItems = [
+  {
+    label: "Feature flags",
+    value: "feature-flags",
+    icon: "i-ph-flag-duotone",
+  },
+] satisfies AccordionItem[];
+
+const selectedItem = ref<string | null>(null);
+</script>
