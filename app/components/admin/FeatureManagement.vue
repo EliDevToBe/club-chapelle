793a27b844e2f5333df7427b7563dd2a62b8259b
EliDevToBe <158275307+EliDevToBe@users.noreply.github.com>
2026-06-29 04:14:09 +0200

    chore: UI FF bg

--- app/components/admin/FeatureManagement.vue
+++ app/components/admin/FeatureManagement.vue
@@ -41,7 +41,7 @@ const ui = {
   rootWrapper: "flex flex-col gap-3 px-2 pb-2",
   flagsWrapper: "flex flex-col gap-3",
   flagItem:
-    "flex items-center justify-between gap-4 rounded-lg border border-default px-3 py-2",
+    "flex items-center justify-between gap-4 rounded-lg border border-default px-3 py-2 bg-default",
   flagContentWrapper: "min-w-0 flex-1 gap-1 flex flex-col",
 
   flagLabel: "text-sm font-medium",
