ba0be6610d4943dbdddf8b88c8ea9cc59e7223bd
EliDevToBe <158275307+EliDevToBe@users.noreply.github.com>
2026-07-04 01:11:38 +0200

    fix accordion ui

--- app/components/ui/ChapAccordion.vue
+++ app/components/ui/ChapAccordion.vue
@@ -4,6 +4,7 @@
     :items="items"
     :class="ui.accordion"
     trailing-icon="i-ph-caret-right-duotone"
+    @update:model-value="(value) => (isOpen = !!value)"
   >
     <template #content="{ item }">
       <slot name="content" :item="item" />
@@ -13,19 +14,21 @@
 
 <script setup lang="ts">
 import type { AccordionItem } from "@nuxt/ui";
+import { LIGHT_BACKGROUND_COLOR } from "~/components/ui/colors";
 
-const props = withDefaults(
-  defineProps<{
-    items: AccordionItem[];
-  }>(),
-  {
-    items: () => [],
-  },
-);
+const props = defineProps<{
+  items: AccordionItem[];
+}>();
 
-const ui = {
-  accordion: "rounded-lg border border-default p-4",
-};
+const isOpen = ref<boolean>(false);
+
+const ui = computed(() => ({
+  accordion: [
+    "rounded-lg border border-default p-4",
+    isOpen.value ? `${LIGHT_BACKGROUND_COLOR}` : "",
+  ],
+}));
+
 const optionsUi = {
   trigger: "cursor-pointer",
   trailingIcon: "group-data-[state=open]:rotate-90",
