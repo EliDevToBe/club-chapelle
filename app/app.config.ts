509949106d4af44b5f60d8cee0e47876f184cf07
EliDevToBe <158275307+EliDevToBe@users.noreply.github.com>
2026-07-04 13:16:40 +0200

    ui: Fix layout footer spaces

--- app/app.config.ts
+++ app/app.config.ts
@@ -17,6 +17,18 @@ export default defineAppConfig({
       },
     },
 
+    footer: {
+      slots: {
+        root: '',
+        top: 'py-8 lg:py-12',
+        bottom: 'py-8 lg:py-12',
+        container: 'py-8 lg:py-4 lg:flex lg:items-center lg:justify-between lg:gap-x-3',
+        left: 'flex items-center justify-center lg:justify-start lg:flex-1 gap-x-1.5 mt-1 lg:mt-0 lg:order-1',
+        center: 'mt-1 lg:mt-0 lg:order-2 flex items-center justify-center',
+        right: 'lg:flex-1 flex items-center justify-center lg:justify-end gap-x-1.5 lg:order-3'
+      }
+    },
+
     navigationMenu: {
       compoundVariants: [
         {
